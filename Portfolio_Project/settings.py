f9097d4bf804ba52b110ebee1934d881c8c1bfef
Joshua Olugbemi <fijacks2005@yahoo.com>
2026-01-20 13:50:14 +0100

    Fix database ssl config for local and production

--- Portfolio_Project/settings.py
+++ Portfolio_Project/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
     'default': dj_database_url.config (
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True,
+        ssl_require=not DEBUG,
     )
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
